e7e23f1f1c6258aee945895231d1a22313dec383
GvMazzon25 <gvmazzon25@gmail.com>
2021-12-16 19:53:51 +0100

    variable

--- index.php
+++ index.php
@@ -4,9 +4,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>PHP Badwords</title>
 </head>
 <body>
-    
+    <?php 
+       $frase=" sono Giorgio e sto facendo l'esercizio di boolean"
+    ?>
+    <?php ?>
+    <h1>Ciao <?php echo $frase?></h1>
 </body>
 </html>
